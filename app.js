48895951853c501640df8a9a105aa1d164f7f4db
sugyan <sugi1982@gmail.com>
2011-01-12 01:02:17 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var conf = require('node-config');
+conf.initConfig(function(err) {
+    if (err) throw err;
+
+    require('./lib/server')(conf).listen(conf.port, conf.host);
+    console.log('Server running at http://' + conf.host + ':' + conf.port + '/');
+});
---
+++ conf/common.js
@@ -0,0 +1,4 @@
+exports.conf = {
+    host: '127.0.0.1',
+    port: 80
+};
