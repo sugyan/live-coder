2545d07f5a949e16d48c057c8a0d0fe0fb5a7a1f
sugyan <sugi1982@gmail.com>
2011-01-12 03:18:02 +0900

    send diff

--- static/js/index.js
+++ static/js/index.js
@@ -2,14 +2,27 @@ $(function() {
     var socket = new io.Socket();
     socket.connect();
     socket.on('connect', function() {
-        console.log('connect');
-    });
-    var editor = new CodeMirror(document.body, {
-        parserfile: 'parsedummy.js',
-        stylesheet: '/css/code.css',
-        path: '/js/lib/codemirror/',
-        onLoad: function() {
-            editor.focus();
-        }
+        var prev;
+        var diff   = new Diff();
+        var editor = new CodeMirror(document.body, {
+            parserfile: 'parsedummy.js',
+            stylesheet: '/css/code.css',
+            path: '/js/lib/codemirror/',
+            onLoad: function() {
+                prev = editor.getCode();
+                editor.focus();
+                socket.on('message', function(msg) {
+                    console.log(msg);
+                });
+            },
+            onCursorActivity: function() {
+                var code  = editor.getCode();
+                var diffs = diff.diff(prev.split('\n'), code.split('\n'));
+                if (diffs.length > 0) {
+                    socket.send(diffs);
+                    prev = code;
+                }
+            }
+        });
     });
 });
