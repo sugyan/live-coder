9f978545c5c1361a86fcda8fcc0602dacf8d1576
sugyan <sugi1982@gmail.com>
2011-02-07 02:08:08 +0900

    send chat message

--- static/js/user.js
+++ static/js/user.js
@@ -1,5 +1,27 @@
 $(function() {
+    var pathname = window.location.pathname;
+    var username = pathname.replace(/\/user\//, '');
     SessionWebSocket(function(socket) {
+        socket.send({ connect: username });
+        socket.on('message', function(msg) {
+            if (msg.chat) {
+                console.log('chat');
+                console.log(msg);
+            }
+        });
+
+        var message = $(':input[name=chat_message]');
+        message.focus();
+        $('#chat_form').submit(function() {
+            socket.send({
+                chat: {
+                    user: username,
+                    data: message.val()
+                }
+            });
+            message.val('');
+            return false;
+        });
     });
     var editor = null;
     editor = new eclipse.Editor({
