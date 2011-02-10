2bf83b9c805e790ef6d95e1346929a8e7ab43f8c
sugyan <sugi1982@gmail.com>
2011-02-11 01:20:33 +0900

    chat処理 共通化

--- static/js/edit.js
+++ static/js/edit.js
@@ -1,33 +1,7 @@
 $(function() {
     SessionWebSocket(function(socket) {
         socket.send({ connect: null });
-        socket.on('message', function(msg) {
-            if (msg.chat) {
-                var messages = $('#messages');
-                var chat = msg.chat;
-                messages.prepend(
-                    $('<dt>')
-                        .append($('<span>').addClass('datetime').text(msg.chat.time))
-                        .append(' - ')
-                        .append($('<span>').addClass('username').text(msg.chat.user))
-                        .after($('<dd>')
-                               .append($('<span>').addClass('message').text(msg.chat.data))));
-            }
-        });
-
-        var message = $(':input[name=chat_message]');
-        message.focus();
-        $('#chat_form').submit(function() {
-            if (! message.val())            return false;
-            if (message.val().length > 140) return false;
-            socket.send({
-                chat: {
-                    data: message.val()
-                }
-            });
-            message.val('');
-            return false;
-        });
+        enableChat(socket);
     });
     var editor = null;
     editor = new eclipse.Editor({
--- static/js/user.js
+++ static/js/user.js
@@ -3,34 +3,7 @@ $(function() {
     var username = pathname.replace(/\/user\//, '');
     SessionWebSocket(function(socket) {
         socket.send({ connect: username });
-        socket.on('message', function(msg) {
-            if (msg.chat) {
-                var messages = $('#messages');
-                var chat = msg.chat;
-                messages.prepend(
-                    $('<dt>')
-                        .append($('<span>').addClass('datetime').text(msg.chat.time))
-                        .append(' - ')
-                        .append($('<span>').addClass('username').text(msg.chat.user))
-                        .after($('<dd>')
-                               .append($('<span>').addClass('message').text(msg.chat.data))));
-            }
-        });
-
-        var message = $(':input[name=chat_message]');
-        message.focus();
-        $('#chat_form').submit(function() {
-            if (! message.val())            return false;
-            if (message.val().length > 140) return false;
-            socket.send({
-                chat: {
-                    user: username,
-                    data: message.val()
-                }
-            });
-            message.val('');
-            return false;
-        });
+        enableChat(socket, username);
     });
     var editor = null;
     editor = new eclipse.Editor({
