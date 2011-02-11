ca8300fafcb894f6d342d4cf7d78d9f76b34af20
sugyan <sugi1982@gmail.com>
2011-02-11 15:09:04 +0900

    edit -> user syncing

--- static/js/edit.js
+++ static/js/edit.js
@@ -1,8 +1,6 @@
 $(function() {
-    SessionWebSocket(function(socket) {
-        socket.send({ connect: null });
-        enableChat(socket);
-    });
+    var max_row = 20;
+    var max_col = 100;
     var editor = null;
     editor = new eclipse.Editor({
         parent: 'code',
@@ -14,5 +12,36 @@ $(function() {
     });
     var styler = new eclipse.TextStyler(editor, 'js');
     editor.setText('');
-    editor.focus();
+    SessionWebSocket(function(socket) {
+        var prev = '';
+        var dmp  = new diff_match_patch();
+        socket.send({ connect: null });
+        enableChat(socket);
+        editor.focus();
+        editor.addEventListener('Modify', this, function() {
+            var model = editor.getModel();
+            var line_count = model.getLineCount();
+            if (line_count > max_row) {
+                model.setText('', model.getLineStart(max_row) - 1);
+            }
+            for (var i = 0; i < model.getLineCount(); i++) {
+                var line = model.getLine(i);
+                if (line.length > max_col) {
+                    var caret = editor.getCaretOffset();
+                    model.setText(line.substring(0, max_col - 1), model.getLineStart(i), model.getLineStart(i) + line.length - 1);
+                    editor.setCaretOffset(caret);
+                }
+            }
+            onActivity();
+        });
+        function onActivity() {
+            var code = editor.getText();
+            var patch = dmp.patch_make(prev, code);
+            if (patch.length > 0) {
+                socket.send({ patch: dmp.patch_toText(patch) });
+                prev = code;
+            }
+            return false;
+        }
+    });
 });
--- static/js/user.js
+++ static/js/user.js
@@ -2,8 +2,16 @@ $(function() {
     var pathname = window.location.pathname;
     var username = pathname.replace(/\/user\//, '');
     SessionWebSocket(function(socket) {
+        var dmp = new diff_match_patch();
         socket.send({ connect: username });
         enableChat(socket, username);
+        socket.on('message', function(msg) {
+            if (msg.patch) {
+                var patch  = dmp.patch_fromText(msg.patch);
+                var result = dmp.patch_apply(patch, editor.getText());
+                editor.setText(result[0]);
+            }
+        });
     });
     var editor = null;
     editor = new eclipse.Editor({
@@ -15,5 +23,6 @@ $(function() {
             '/css/js.css'
         ]
     });
+    var styler = new eclipse.TextStyler(editor, 'js');
     editor.setText('');
 });
