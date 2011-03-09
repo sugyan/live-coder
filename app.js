dbb42b68646e76eed9d00fc6a0e14a7589efe113
sugyan <sugi1982@gmail.com>
2011-03-09 15:44:46 +0900

    add header, footer, and about page

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ var common  = config('common', {
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+app.helpers({
+    jss: []
+});
 
 app.get('/', function(req, res) {
     res.render('index', { jss: ['/js/index.js'] });
+});
+app.get('/about', function(req, res) {
+    res.render('about');
 });
 app.listen(common.port, common.host);
 
