1675985065e74ad4e892d24ddbf83d6e506cf75c
sugyan <sugi1982@gmail.com>
2011-03-09 15:10:50 +0900

    add editor

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,9 @@
 $(function() {
+    var editor = new eclipse.Editor({
+        parent: 'code',
+        model: new eclipse.TextModel(),
+        stylesheet: '/css/code.css'
+    });
+    editor.setText('');
+    editor.focus();
 });
