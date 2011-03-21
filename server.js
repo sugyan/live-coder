46b90728bdfb613aeedaba484f301537bd0c9de3
sugyan <sugi1982@gmail.com>
2011-03-22 00:59:08 +0900

    share connect-mongodb store

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
-var http = require('./lib/http');
-require('./lib/socket.io')(http);
+// share session with http server and socket.io
+var store = new require('connect-mongodb')();
+
+var http = require('./lib/http')(store);
+require('./lib/socket.io')(http, store);
