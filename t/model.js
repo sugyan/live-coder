e730f7be4d025833c4ba958d5bd80b89fac62233
sugyan <sugi1982@gmail.com>
2011-04-03 03:49:18 +0900

    detect empty port

--- t/model.js
+++ t/model.js
@@ -1,133 +1,134 @@
 require('../test_helper');
 require('../lib/model');
 
-var mongod;
-var port = 17017;               // empty port...?
-QUnit.module('model', {
-    setup: function() {
-        mongod = require('child_process').spawn('mongod', [
-            '--dbpath', 't/db',
-            '--port', port,
-            '--nounixsocket', '-v'
-        ]);
-        // mongod.stdout.on('data', function(data) {
-        //     console.log(data.toString());
-        // });
-    },
-    teardown: function() {
-        mongod.kill();
-    }
-});
-
-QUnit.test('mongoose', function() {
-    var mongoose = require('mongoose');
-
-    var User = mongoose.model('User');
-    var Auth = mongoose.model('Auth');
-    assert.ok(mongoose, 'mongoose is available');
-    assert.ok(User, '"User" model is available');
-    assert.ok(Auth, '"Auth" model is available');
-
-    QUnit.stop();
-
-    setTimeout(function() {
-        mongoose.connect('localhost', 'test', port, function(err) {
-            QUnit.start();
-            assert.equal(err, null, 'connect success');
-        });
-    }, 500);
-
-    QUnit.stop();
-
-    var key = 'hoge';
-    var async = require('async');
-    async.series([
-        // initialization
-        function(cb) {
-            User.remove({}, function(err) {
-                assert.equal(err, null, 'user removed');
-                User.find({}, function(err, docs) {
-                    assert.equal(err, null, 'find users');
-                    assert.equal(docs.length, 0, 'no users');
-                    cb(err);
-                });
-            });
+empty_port(function(port) {
+    var mongod;
+    QUnit.module('model', {
+        setup: function() {
+            mongod = require('child_process').spawn('mongod', [
+                '--dbpath', 't/db',
+                '--port', port,
+                '--nounixsocket', '-v'
+            ]);
+            // mongod.stdout.on('data', function(data) {
+            //     console.log(data.toString());
+            // });
         },
-        function(cb) {
-            Auth.remove({}, function(err) {
-                assert.equal(err, null, 'auth removed');
-                Auth.find({}, function(err, docs) {
-                    assert.equal(err, null, 'find auths');
-                    assert.equal(docs.length, 0, 'no auths');
-                    cb(err);
-                });
-            });
-        },
-        // new user
-        function(cb) {
-            User.find_or_create(
-                { key: 'hoge', name: 'sugyan', info: { foo: 'bar' } },
-                function(err, result) {
-                    assert.ok(result.user, 'user created');
-                    cb(err);
-                }
-            );
-        },
-        // 1 auth, 1 user
-        function(cb) {
-            Auth.find({}, function(err, docs) {
-                assert.equal(docs.length, 1, '1 auths');
-                User.find({}, function(err, docs) {
-                    assert.equal(docs.length, 1, '1 users');
-                    cb(err);
-                });
-            });
-        },
-        // same name user
-        function(cb) {
-            User.find_or_create(
-                { key: 'fuga', name: 'sugyan', info: { foo: 'bar' } },
-                function(err, result) {
-                    assert.ok(result.user, 'another user created');
-                    cb(err);
-                }
-            );
-        },
-        // 2 auth, 2 user
-        function(cb) {
-            Auth.find({}, function(err, docs) {
-                assert.equal(docs.length, 2, '2 auths');
-                User.find({}, function(err, docs) {
-                    assert.equal(docs.length, 2, '2 users');
-                    cb(err);
-                });
-            });
-        },
-        // find user
-        function(cb) {
-            User.find_or_create(
-                { key: 'hoge', name: 'dummy', info: { dummy: 'dummy' } },
-                function(err, result) {
-                    assert.ok(result.user, 'user found');
-                    assert.equal(result.user.name, 'sugyan', 'user name');
-                    cb(err);
-                }
-            );
-        },
-        // 2 auth, 2 user (not created)
-        function(cb) {
-            Auth.find({}, function(err, docs) {
-                assert.equal(docs.length, 2, '2 auths');
-                User.find({}, function(err, docs) {
-                    assert.equal(docs.length, 2, '2 users');
-                    cb(err);
-                });
-            });
+        teardown: function() {
+            mongod.kill();
         }
-    ], function(err) {
-        assert.equal(err, null, 'no errors occurred');
-        QUnit.start();
     });
-});
 
-QUnit.start();
+    QUnit.test('mongoose', function() {
+        var mongoose = require('mongoose');
+
+        var User = mongoose.model('User');
+        var Auth = mongoose.model('Auth');
+        assert.ok(mongoose, 'mongoose is available');
+        assert.ok(User, '"User" model is available');
+        assert.ok(Auth, '"Auth" model is available');
+
+        QUnit.stop();
+
+        setTimeout(function() {
+            mongoose.connect('localhost', 'test', port, function(err) {
+                QUnit.start();
+                assert.equal(err, null, 'connect success');
+            });
+        }, 500);
+
+        QUnit.stop();
+
+        var key = 'hoge';
+        var async = require('async');
+        async.series([
+            // initialization
+            function(cb) {
+                User.remove({}, function(err) {
+                    assert.equal(err, null, 'user removed');
+                    User.find({}, function(err, docs) {
+                        assert.equal(err, null, 'find users');
+                        assert.equal(docs.length, 0, 'no users');
+                        cb(err);
+                    });
+                });
+            },
+            function(cb) {
+                Auth.remove({}, function(err) {
+                    assert.equal(err, null, 'auth removed');
+                    Auth.find({}, function(err, docs) {
+                        assert.equal(err, null, 'find auths');
+                        assert.equal(docs.length, 0, 'no auths');
+                        cb(err);
+                    });
+                });
+            },
+            // new user
+            function(cb) {
+                User.find_or_create(
+                    { key: 'hoge', name: 'sugyan', info: { foo: 'bar' } },
+                    function(err, result) {
+                        assert.ok(result.user, 'user created');
+                        cb(err);
+                    }
+                );
+            },
+            // 1 auth, 1 user
+            function(cb) {
+                Auth.find({}, function(err, docs) {
+                    assert.equal(docs.length, 1, '1 auths');
+                    User.find({}, function(err, docs) {
+                        assert.equal(docs.length, 1, '1 users');
+                        cb(err);
+                    });
+                });
+            },
+            // same name user
+            function(cb) {
+                User.find_or_create(
+                    { key: 'fuga', name: 'sugyan', info: { foo: 'bar' } },
+                    function(err, result) {
+                        assert.ok(result.user, 'another user created');
+                        cb(err);
+                    }
+                );
+            },
+            // 2 auth, 2 user
+            function(cb) {
+                Auth.find({}, function(err, docs) {
+                    assert.equal(docs.length, 2, '2 auths');
+                    User.find({}, function(err, docs) {
+                        assert.equal(docs.length, 2, '2 users');
+                        cb(err);
+                    });
+                });
+            },
+            // find user
+            function(cb) {
+                User.find_or_create(
+                    { key: 'hoge', name: 'dummy', info: { dummy: 'dummy' } },
+                    function(err, result) {
+                        assert.ok(result.user, 'user found');
+                        assert.equal(result.user.name, 'sugyan', 'user name');
+                        cb(err);
+                    }
+                );
+            },
+            // 2 auth, 2 user (not created)
+            function(cb) {
+                Auth.find({}, function(err, docs) {
+                    assert.equal(docs.length, 2, '2 auths');
+                    User.find({}, function(err, docs) {
+                        assert.equal(docs.length, 2, '2 users');
+                        cb(err);
+                    });
+                });
+            }
+        ], function(err) {
+            assert.equal(err, null, 'no errors occurred');
+            QUnit.start();
+        });
+    });
+
+    QUnit.start();
+});
--- test_helper.js
+++ test_helper.js
@@ -14,3 +14,33 @@ QUnit.config.updateRate = 0;
 QUnit.tap.showDetailsOnFailure = true;
 
 exports.assert = QUnit;
+
+exports.empty_port = function(callback) {
+    port = 10000 + Math.floor(Math.random() * 1000);
+
+    var net = require('net');
+    var socket = new net.Socket();
+    var server = new net.Server();
+
+    socket.on('error', function(e) {
+        try {
+            server.listen(port, '127.0.0.1');
+            server.close();
+            callback(port);
+        } catch(e) {
+            loop();
+        };
+    });
+    function loop() {
+        if (port++ >= 20000) {
+            callback(null);
+            return;
+        }
+
+        socket.connect(port, '127.0.0.1', function() {
+            socket.destroy();
+            loop();
+        });
+    };
+    loop();
+};
