5f4a7dca50ecf0397dac8afb1ef0f48caaff99f5
sugyan <sugi1982@gmail.com>
2011-04-09 13:29:04 +0900

    connect-mongodb initialization

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // share session with http server and socket.io
-var store = new require('connect-mongodb')();
+var store = require('connect-mongodb')();
 
 var http = require('./lib/http')(store);
 require('./lib/socket.io')(http, store);
