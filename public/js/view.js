f60c0a443c42ff47ca85eb9d4d67ab934421f393
sugyan <sugi1982@gmail.com>
2011-04-09 17:57:39 +0900

    view page

---
+++ public/js/view.js
@@ -0,0 +1,34 @@
+$(function() {
+    var dmp = new diff_match_patch(),
+        socket = new io.Socket(),
+        editor = new eclipse.Editor({
+            parent: 'code',
+            model: new eclipse.TextModel(),
+            stylesheet: '/css/code.css',
+            readonly: true
+        });
+    editor.setText('');
+    $('#cursor').offset({ top: 60, left: 20 }).height(editor.getLineHeight());
+
+    socket.on('message', function(msg) {
+        if (msg.patch) {
+            var patches = dmp.patch_fromText(msg.patch);
+            var results = dmp.patch_apply(patches, editor.getText());
+            // TODO: check results
+            editor.setText(results[0]);
+        }
+        if (msg.cursor) {
+            var offset = editor.getModel().getLineStart(msg.cursor.row) + msg.cursor.col;
+            var location = editor.getLocationAtOffset(offset);
+            $('#cursor').show().offset({ top : 60 + location.y, left: 20 + location.x });
+        }
+    });
+    socket.connect();
+
+    var blink = function() {
+        var cursor = $('#cursor');
+        cursor[cursor.css('display') === 'none' ? 'show' : 'hide']();
+        setTimeout(blink, 500);
+    };
+    blink();
+});
