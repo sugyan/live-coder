02d4a7a5a76bc7d7a818c2141083047f0807e9c6
sugyan <sugi1982@gmail.com>
2011-05-09 18:51:01 +0900

    use npm 1.0

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 git submodule update --init
+npm install config
 npm install express
-npm install socket.io
-npm install mongoose
 npm install connect-mongodb
 npm install oauth
+npm install socket.io
 npm install ejs
-npm install async
-npm install config
+npm install mongoose
